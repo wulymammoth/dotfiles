c3863c190483bcef446dbbeb1e030fe5021aa79e
wulymammoth <861147+wulymammoth@users.noreply.github.com>
2021-12-27 14:20:36 -0500

    FORMAT

--- nvim/.config/nvim/plugin/cmp.lua
+++ nvim/.config/nvim/plugin/cmp.lua
@@ -1,14 +1,14 @@
-local cmp = require'cmp'
+local cmp     = require('cmp')
 local lspkind = require('lspkind')
 
 cmp.setup({
   formatting = {
     format = lspkind.cmp_format({
       menu = {
-        buffer   = "[buf]",
-        nvim_lsp = "[LSP]",
-        path     = "[path]",
-        luasnip  = "[luasnip]",
+        buffer   = '[buf]',
+        nvim_lsp = '[LSP]',
+        path     = '[path]',
+        luasnip  = '[luasnip]',
       },
       maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
       with_text = true,
@@ -20,9 +20,9 @@ cmp.setup({
     ['<C-f>']     = cmp.mapping(cmp.mapping.scroll_docs(4), { 'i', 'c' }),
     ['<C-Space>'] = cmp.mapping(cmp.mapping.complete(), { 'i', 'c' }),
     ['<C-e>']     = cmp.mapping({ i = cmp.mapping.abort(), c = cmp.mapping.close() }),
-    ["<c-y>"]     = cmp.mapping(
+    ['<c-y>']     = cmp.mapping(
       cmp.mapping.confirm { behavior = cmp.ConfirmBehavior.Insert, select = true },
-      { "i", "c" }
+      { 'i', 'c' }
     ),
     ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
   },
--- nvim/.config/nvim/plugin/lsp-installer.lua
+++ nvim/.config/nvim/plugin/lsp-installer.lua
@@ -3,8 +3,8 @@ local lsp_installer = require("nvim-lsp-installer")
 lsp_installer.settings({
   ui = {
     icons = {
-      server_installed = "✓",
-      server_pending = "➜",
+      server_installed   = "✓",
+      server_pending     = "➜",
       server_uninstalled = "✗"
     }
   }
@@ -14,22 +14,10 @@ local server_opts = {
   sumneko_lua = {
     settings = {
       Lua = {
-        runtime = {
-          -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
-          version = 'LuaJIT',
-        },
-        diagnostics = {
-          -- Get the language server to recognize the `vim` global
-          globals = {'vim'},
-        },
-        workspace = {
-          -- Make the server aware of Neovim runtime files
-          library = vim.api.nvim_get_runtime_file("", true),
-        },
-        -- Do not send telemetry data containing a randomized but unique identifier
-        telemetry = {
-          enable = false,
-        },
+        runtime     = { version = 'LuaJIT' },
+        diagnostics = { globals = {'vim'} },
+        workspace   = { library = vim.api.nvim_get_runtime_file("", true) },
+        telemetry   = { enable = false },
       },
     },
   }
@@ -45,7 +33,7 @@ local on_attach = function(_client, bufnr)
   buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
 
   -- Mappings.
-  local opts = { noremap=true, silent=true }
+  local opts = { noremap = true, silent = true }
 
   -- See `:help vim.lsp.*` for documentation on any of the below functions
   buf_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
