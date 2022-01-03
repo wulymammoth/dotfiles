3dc4979444c6407e2bc83263678e56f034aedad0
wulymammoth <861147+wulymammoth@users.noreply.github.com>
2022-01-03 02:15:49 -0800

    UPDATE nvim - cinfigure indent-blankline not to underline

--- nvim/.config/nvim/after/plugin/indent-blankline.lua
+++ nvim/.config/nvim/after/plugin/indent-blankline.lua
@@ -1,7 +1,3 @@
 vim.opt.list = true
 
-require("indent_blankline").setup {
-  show_current_context = true,
-  show_current_context_start = true,
-  show_end_of_line = true,
-}
+require("indent_blankline").setup { show_current_context = true }
