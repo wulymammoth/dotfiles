73044b4e534f4360c22badc192d8201c99f3a13e
David Wu <dwu@easypost.com>
2022-01-05 18:34:03 +0000

    UPDATE treesitter config - installed langs

--- nvim/.config/nvim/after/plugin/treesitter.lua
+++ nvim/.config/nvim/after/plugin/treesitter.lua
@@ -6,6 +6,8 @@ require'nvim-treesitter.configs'.setup {
     -- `false` will disable the whole extension
     enable = true,
 
+    ensure_installed = { 'python', 'ruby', 'rust' },
+
     -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
     -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
     -- Using this option may slow down your editor, and you may see some duplicate highlights.
