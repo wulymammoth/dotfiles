d70b52f1530c21782b063a417f96366aea521f7c
wulymammoth <861147+wulymammoth@users.noreply.github.com>
2022-03-22 12:23:52 -0400

    UPDATE nvim lualine config to be ready for global statusline in nvim

--- nvim/.config/nvim/after/plugin/lualine.lua
+++ nvim/.config/nvim/after/plugin/lualine.lua
@@ -6,6 +6,7 @@ require'lualine'.setup {
     section_separators   = { left = '', right = ''},
     disabled_filetypes   = {},
     always_divide_middle = true,
+    -- globalstatus         = true, TODO: uncomment when 0.7 is released or use nightly
   },
   sections = {
     lualine_a = {'mode'},
