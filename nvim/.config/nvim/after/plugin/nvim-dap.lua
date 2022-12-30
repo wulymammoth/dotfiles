68b0d8cb1566098d9ad8e440a987118865507415
wulymammoth <861147+wulymammoth@users.noreply.github.com>
2022-12-30 15:57:01 -0500

    UPDATE (nvim) dapui config

--- nvim/.config/nvim/after/plugin/nvim-dap.lua
+++ nvim/.config/nvim/after/plugin/nvim-dap.lua
@@ -71,12 +71,12 @@ dapui.setup({
 dap.listeners.after.event_initialized["dapui_config"] = function()
   dapui.open()
 end
-dap.listeners.before.event_terminated["dapui_config"] = function()
-  dapui.close()
-end
-dap.listeners.before.event_exited["dapui_config"] = function()
-  dapui.close()
-end
+-- dap.listeners.before.event_terminated["dapui_config"] = function()
+--   dapui.close()
+-- end
+-- dap.listeners.before.event_exited["dapui_config"] = function()
+--   dapui.close()
+-- end
 
 -- elixir
 dap.adapters.mix_task = {
