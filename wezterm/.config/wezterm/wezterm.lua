297bdde1b79fe2eea8fe893c9887dd1b83fcfc0f
wulymammoth <861147+wulymammoth@users.noreply.github.com>
2023-02-04 08:37:09 -0500

    ADD wezterm

---
+++ wezterm/.config/wezterm/wezterm.lua
@@ -0,0 +1,15 @@
+local wezterm = require 'wezterm';
+
+return {
+  color_scheme = "iceberg-dark",
+  font_size = 13.5,
+  keys = {
+    -- Make Option-Left equivalent to Alt-b which many line editors interpret as backward-word
+    { key = "_", mods = "SHIFT|CTRL", action = wezterm.action.DisableDefaultAssignment },
+    { key="b", mods="OPT", action=wezterm.action{ SendString="\x1bb" } },
+    -- Make Option-Right equivalent to Alt-f; forward-word
+    { key="f", mods="OPT", action=wezterm.action{ SendString="\x1bf" } },
+    -- { key = 'Enter', mods = 'CTRL|OPT', action = wezterm.action.DisableDefaultAssignment },
+  },
+  window_decorations = "NONE"
+}
