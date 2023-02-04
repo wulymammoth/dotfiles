91b1dcdbb2fe794a2d94faf067590233ac105f59
wulymammoth <861147+wulymammoth@users.noreply.github.com>
2023-02-04 10:05:41 -0500

    UPDATE (wezterm) unbind caret

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -7,14 +7,20 @@ return {
   font_size = 14.5,
 
   keys = {
+    -- Give me my Vim binding
+    { key = '^', mods = 'SHIFT|CTRL', action = wezterm.action.DisableDefaultAssignment },
+
+    -- Give me my undo
     { key = "_", mods = "SHIFT|CTRL", action = wezterm.action.DisableDefaultAssignment },
+
     -- Make Option-Left equivalent to Alt-b which many line editors interpret as backward-word
     { key="b", mods="OPT", action=wezterm.action{ SendString="\x1bb" } },
+
     -- delete forward
     { key="d", mods="OPT", action=wezterm.action{ SendString="\x1bd" } },
+
     -- Make Option-Right equivalent to Alt-f; forward-word
     { key="f", mods="OPT", action=wezterm.action{ SendString="\x1bf" } },
-    -- { key = 'Enter', mods = 'CTRL|OPT', action = wezterm.action.DisableDefaultAssignment },
   },
 
   window_decorations = "NONE"
