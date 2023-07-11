86b8fb84e1c1d0ce6601c66206249298436e37cb
wulymammoth <861147+wulymammoth@users.noreply.github.com>
2023-07-11 00:39:28 -0400

    UPDATE nvim - global status line

--- nvim/.config/nvim/after/plugin/lualine.lua
+++ nvim/.config/nvim/after/plugin/lualine.lua
@@ -6,7 +6,7 @@ require'lualine'.setup {
     section_separators   = { left = '', right = ''},
     disabled_filetypes   = {},
     always_divide_middle = true,
-    -- globalstatus         = true, TODO: uncomment when 0.7 is released or use nightly
+    globalstatus         = true,
   },
   sections = {
     lualine_a = {'mode'},
