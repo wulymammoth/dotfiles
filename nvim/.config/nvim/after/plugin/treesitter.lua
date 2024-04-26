634ea0ebbb3897b096f7bd7ab52f69ab1c093a14
wulymammoth <861147+wulymammoth@users.noreply.github.com>
2024-04-26 10:58:49 -0400

    UPDATE nvim - disable treesitter when files are massive

--- nvim/.config/nvim/after/plugin/treesitter.lua
+++ nvim/.config/nvim/after/plugin/treesitter.lua
@@ -50,6 +50,11 @@ require'nvim-treesitter.configs'.setup {
     -- Using this option may slow down your editor, and you may see some duplicate highlights.
     -- Instead of true it can also be a list of languages
     additional_vim_regex_highlighting = false,
+
+    -- this shit is painful when loading massive files
+    disable = function(lang, bufnr) -- Disable in files with more than 5K
+        return vim.api.nvim_buf_line_count(bufnr) > 1800
+    end,
   },
 
   indent = {
