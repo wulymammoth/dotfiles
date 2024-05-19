199cd38dab1bb01276ee46baaff40f8a92f2327d
wulymammoth <861147+wulymammoth@users.noreply.github.com>
2024-05-19 15:44:56 -0400

    UPDATE nvim - disable No Name buffer when opening dirs in nvim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,6 +1,7 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
+vim.opt.hidden = false -- disable 'No Name' buffer
 vim.opt.number = true -- Optionally, also enable absolute line numbers for the current line
 vim.opt.relativenumber = true -- Enable relative line numbers
 vim.opt.swapfile = false -- disable swapfile
