a08c49ff41efada4cf31fd2e2ccd5096f60cbd8e
wulymammoth <861147+wulymammoth@users.noreply.github.com>
2024-05-19 15:01:35 -0400

    UPDATE nvim - disable swapfile

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,8 +1,6 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
--- Enable relative line numbers
-vim.opt.relativenumber = true
-
--- Optionally, also enable absolute line numbers for the current line
-vim.opt.number = true
+vim.opt.number = true -- Optionally, also enable absolute line numbers for the current line
+vim.opt.relativenumber = true -- Enable relative line numbers
+vim.opt.swapfile = false -- disable swapfile
