32016722f8f072c71881edac5491a7cedaa2fe2a
wulymammoth <861147+wulymammoth@users.noreply.github.com>
2024-05-21 08:02:19 -0400

    UPDATE nvim - colorscheme to nordic

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,14 +1,13 @@
 return {
+  { "AlexvZyl/nordic.nvim" },
   { "EdenEast/nightfox.nvim" },
   { "catppuccin/nvim" },
-  { "navarasu/onedark.nvim" },
   { "rebelot/kanagawa.nvim" },
-  { "rose-pine/neovim" },
 
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "kanagawa",
+      colorscheme = "nordic",
     },
   },
 }
