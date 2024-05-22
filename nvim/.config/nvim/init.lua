956dfa5b63c1ea9d6840ada9eb4546b1219119c3
wulymammoth <861147+wulymammoth@users.noreply.github.com>
2024-05-22 07:49:54 -0400

    UPDATE nvim - noisy python diagnostics

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,6 +1,15 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
+vim.diagnostic.config({
+  virtual_text = false, -- Disable inline diagnostics
+  signs = true, -- Keep diagnostics in the gutter
+  underline = true, -- Underline diagnostics in the code (optional)
+  update_in_insert = false, -- Update diagnostics in insert mode (optional)
+})
+
+vim.o.hlsearch = false -- disable highlight search
+
 vim.opt.hidden = false -- disable 'No Name' buffer
 vim.opt.number = true -- Optionally, also enable absolute line numbers for the current line
 vim.opt.relativenumber = true -- Enable relative line numbers
