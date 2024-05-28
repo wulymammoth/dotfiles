3e53476fdceb6493131885a318f6e1c265bf0019
wulymammoth <861147+wulymammoth@users.noreply.github.com>
2024-05-28 12:26:33 -0400

    ADD nvim dep - nvim-treesitter-refactor

---
+++ nvim/.config/nvim/lua/plugins/nvim-treesitter-refactor.lua
@@ -0,0 +1,25 @@
+return {
+  "nvim-treesitter/nvim-treesitter-refactor",
+
+  config = function()
+    require("nvim-treesitter.configs").setup({
+      refactor = {
+        highlight_current_scope = { enable = false },
+
+        highlight_definitions = {
+          enable = true,
+          -- Set to false if you have an `updatetime` of ~100.
+          clear_on_cursor_move = true,
+        },
+
+        smart_rename = {
+          enable = true,
+          -- Assign keymaps to false to disable them, e.g. `smart_rename = false`.
+          keymaps = {
+            smart_rename = "grr",
+          },
+        },
+      },
+    })
+  end,
+}
