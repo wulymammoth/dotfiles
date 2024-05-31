0dab8304857860ecced9daa9f45e5451a84c82c7
wulymammoth <861147+wulymammoth@users.noreply.github.com>
2024-05-31 12:47:19 -0400

    UPDATE wezterm font size

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,36 +1,36 @@
-local wezterm = require 'wezterm';
+local wezterm = require("wezterm")
 
 return {
-  color_scheme = "iceberg-dark",
+	color_scheme = "iceberg-dark",
 
-  font_size = 16,
-  font = wezterm.font('Fira Mono for Powerline', { weight = 'Regular' }),
+	font_size = 15,
+	font = wezterm.font("Fira Mono for Powerline", { weight = "Regular" }),
 
-  front_end = "WebGpu",
+	front_end = "WebGpu",
 
-  hide_tab_bar_if_only_one_tab = true,
+	hide_tab_bar_if_only_one_tab = true,
 
-  keys = {
-    -- Give me my Vim binding
-    { key = '^', mods = 'SHIFT|CTRL', action = wezterm.action.DisableDefaultAssignment },
+	keys = {
+		-- Give me my Vim binding
+		{ key = "^", mods = "SHIFT|CTRL", action = wezterm.action.DisableDefaultAssignment },
 
-    -- Give me my undo
-    { key = "_", mods = "SHIFT|CTRL", action = wezterm.action.DisableDefaultAssignment },
+		-- Give me my undo
+		{ key = "_", mods = "SHIFT|CTRL", action = wezterm.action.DisableDefaultAssignment },
 
-    -- Make Option-Left equivalent to Alt-b which many line editors interpret as backward-word
-    { key="b", mods="OPT", action=wezterm.action{ SendString="\x1bb" } },
+		-- Make Option-Left equivalent to Alt-b which many line editors interpret as backward-word
+		{ key = "b", mods = "OPT", action = wezterm.action({ SendString = "\x1bb" }) },
 
-    -- delete forward
-    { key="d", mods="OPT", action=wezterm.action{ SendString="\x1bd" } },
+		-- delete forward
+		{ key = "d", mods = "OPT", action = wezterm.action({ SendString = "\x1bd" }) },
 
-    -- Make Option-Right equivalent to Alt-f; forward-word
-    { key="f", mods="OPT", action=wezterm.action{ SendString="\x1bf" } },
-  },
+		-- Make Option-Right equivalent to Alt-f; forward-word
+		{ key = "f", mods = "OPT", action = wezterm.action({ SendString = "\x1bf" }) },
+	},
 
-  use_fancy_tab_bar = false,
+	use_fancy_tab_bar = false,
 
-  use_resize_increments = true,
+	use_resize_increments = true,
 
-  window_close_confirmation = 'AlwaysPrompt',
-  window_decorations = "RESIZE",
+	window_close_confirmation = "AlwaysPrompt",
+	window_decorations = "RESIZE",
 }
