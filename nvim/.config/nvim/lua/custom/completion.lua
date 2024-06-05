263c608218ef82dfe5f49bb66f7b88b12b8b5162
wulymammoth <861147+wulymammoth@users.noreply.github.com>
2024-06-05 00:21:37 -0400

    update nvim-cmp - disable cmdline path

--- nvim/.config/nvim/lua/custom/completion.lua
+++ nvim/.config/nvim/lua/custom/completion.lua
@@ -76,16 +76,16 @@ cmp.setup.cmdline({ "/", "?" }, {
 })
 
 -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline(":", {
-  mapping = cmp.mapping.preset.cmdline(),
-
-  sources = cmp.config.sources({
-    { name = "path" },
-  }, {
-    { name = "cmdline" },
-  }),
-  matching = { disallow_symbol_nonprefix_matching = false },
-})
+-- cmp.setup.cmdline(":", {
+--   mapping = cmp.mapping.preset.cmdline(),
+--
+--   sources = cmp.config.sources({
+--     { name = "path" },
+--   }, {
+--     { name = "cmdline" },
+--   }),
+--   matching = { disallow_symbol_nonprefix_matching = false },
+-- })
 
 -- Set up lspconfig.
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
