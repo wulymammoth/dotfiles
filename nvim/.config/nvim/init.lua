ce7a892226840b4e4f38af24177b8fbd0c8e080d
wulymammoth <861147+wulymammoth@users.noreply.github.com>
2024-06-13 00:42:25 -0400

    UPDATE nvim - init.lua comments

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,13 +1,14 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
+-- set hightlight color and background
 vim.cmd([[highlight Visual ctermfg=white ctermbg=black guifg=white guibg=black]])
 
-vim.g.editorconfig = 1
+vim.g.editorconfig = 1 -- enable editorconfig
 
 vim.o.hlsearch = false -- disable highlight search
 vim.o.winbar = "%=%m %f" -- Set the window bar to display relative path
-vim.o.wrap = true
+vim.o.wrap = true -- enable line wrap
 
 vim.opt.hidden = false -- disable 'No Name' buffer
 vim.opt.number = true -- Optionally, also enable absolute line numbers for the current line
