3f0834be0fadf1f80eb5ea1330e3908ae507b319
wulymammoth <861147+wulymammoth@users.noreply.github.com>
2024-07-06 10:14:34 -0400

    fix(nvim): visual highlight

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,2 +1,4 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
+
+vim.cmd([[highlight Visual ctermfg=white ctermbg=black guifg=white guibg=black]]) -- set hightlight color and background
--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,9 +1,6 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-
-vim.cmd([[highlight Visual ctermfg=white ctermbg=black guifg=white guibg=black]]) -- set hightlight color and background
-
 vim.g.editorconfig = 1 -- enable editorconfig
 vim.g.lazyvim_python_lsp = "pyright"
 vim.g.lazyvim_python_ruff = "ruff_lsp"
