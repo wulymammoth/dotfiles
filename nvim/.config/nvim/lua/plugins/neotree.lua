bfd186228b7188e8ebe8ce1f6c8239e21aa82a69
wulymammoth <861147+wulymammoth@users.noreply.github.com>
2024-07-21 00:05:01 -0400

    disable(nvim): neo-tree auto-open

--- nvim/.config/nvim/lua/plugins/neotree.lua
+++ nvim/.config/nvim/lua/plugins/neotree.lua
@@ -1,6 +1,15 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
 
+  init = function()
+    vim.g.neotree = {
+      auto_close = true,
+      auto_open = false,
+      auto_update = true,
+      update_to_buf_dir = true,
+    }
+  end,
+
   -- merged with the default configuration
   opts = {
     close_if_last_window = true,
