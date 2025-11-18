f389f5182bee2ae0f0b688662567e68b3bee39b3
wulymammoth <861147+wulymammoth@users.noreply.github.com>
2025-11-18 10:23:53 -0500

    fix(nvim): borked inline-completion with stale version of nvim

--- nvim/.config/nvim/lua/plugins/inline-completion.lua
+++ nvim/.config/nvim/lua/plugins/inline-completion.lua
@@ -2,51 +2,26 @@
 -- Based on https://github.com/neovim/neovim/pull/33972
 -- Available API: enable, get, is_enabled, select
 return {
-  {
-    "neovim/nvim-lspconfig",
-    opts = function(_, opts)
-      -- Enable inline completion globally when LSP supports it
-      vim.api.nvim_create_autocmd("LspAttach", {
-        callback = function(args)
-          local client = vim.lsp.get_client_by_id(args.data.client_id)
-          if client and client.supports_method("textDocument/inlineCompletion") then
-            vim.lsp.inline_completion.enable()
-          end
-        end,
-      })
+  "neovim/nvim-lspconfig",
+  enabled = require("lazyvim.util").has("nvim-0.12"),
+  init = function()
+    local inline_completion = vim.lsp and vim.lsp.inline_completion
+    if not inline_completion or type(inline_completion.enable) ~= "function" then
+      -- Older Neovim builds can be < 0.12 even if Lazy thinks otherwise; bail out gracefully.
+      return
+    end
 
-      return opts
-    end,
-    keys = {
-      -- Accept/select inline completion
-      {
-        "<C-y>",
-        function()
-          local completion = vim.lsp.inline_completion.get()
-          if completion then
-            vim.lsp.inline_completion.select()
-            return
-          end
-          return "<C-y>"
-        end,
-        expr = true,
-        mode = "i",
-        desc = "Accept inline completion",
-      },
-      -- Get current inline completion (for debugging)
-      {
-        "<C-x><C-i>",
-        function()
-          local completion = vim.lsp.inline_completion.get()
-          if completion then
-            print("Inline completion available:", vim.inspect(completion))
-          else
-            print("No inline completion available")
-          end
-        end,
-        mode = "i",
-        desc = "Show inline completion info",
-      },
-    },
-  },
+    vim.api.nvim_create_autocmd("LspAttach", {
+      group = vim.api.nvim_create_augroup("kickstart-lsp-attach-inline-completion", { clear = true }),
+      callback = function(args)
+        local client = vim.lsp.get_client_by_id(args.data.client_id)
+        if client and client.server_capabilities.inlineCompletionProvider then
+          inline_completion.enable({
+            debounce = 150,
+            show_on_undo = false,
+          })
+        end
+      end,
+    })
+  end,
 }
