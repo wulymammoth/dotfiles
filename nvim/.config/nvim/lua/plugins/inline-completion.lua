fa327c7f7643a97bef941af127f21423ec98592d
wulymammoth <861147+wulymammoth@users.noreply.github.com>
2026-03-16 22:55:29 -0400

    bugfix(nvim): LSP attach

--- nvim/.config/nvim/lua/plugins/inline-completion.lua
+++ nvim/.config/nvim/lua/plugins/inline-completion.lua
@@ -1,27 +1,30 @@
--- Inline completion configuration for Neovim 0.12.0+
--- Based on https://github.com/neovim/neovim/pull/33972
--- Available API: enable, get, is_enabled, select
-return {
-  "neovim/nvim-lspconfig",
-  enabled = require("lazyvim.util").has("nvim-0.12"),
-  init = function()
-    local inline_completion = vim.lsp and vim.lsp.inline_completion
-    if not inline_completion or type(inline_completion.enable) ~= "function" then
-      -- Older Neovim builds can be < 0.12 even if Lazy thinks otherwise; bail out gracefully.
-      return
-    end
+-- Inline completion configuration for Neovim 0.12.0+.
+-- On older Neovim versions, this file returns no plugin specs.
+if vim.fn.has("nvim-0.12") == 0 then
+  return {}
+end
 
-    vim.api.nvim_create_autocmd("LspAttach", {
-      group = vim.api.nvim_create_augroup("kickstart-lsp-attach-inline-completion", { clear = true }),
-      callback = function(args)
-        local client = vim.lsp.get_client_by_id(args.data.client_id)
-        if client and client.server_capabilities.inlineCompletionProvider then
-          inline_completion.enable({
-            debounce = 150,
-            show_on_undo = false,
-          })
-        end
-      end,
-    })
-  end,
+return {
+  {
+    "folke/snacks.nvim",
+    init = function()
+      local inline_completion = vim.lsp and vim.lsp.inline_completion
+      if not inline_completion or type(inline_completion.enable) ~= "function" then
+        return
+      end
+
+      vim.api.nvim_create_autocmd("LspAttach", {
+        group = vim.api.nvim_create_augroup("kickstart-lsp-attach-inline-completion", { clear = true }),
+        callback = function(args)
+          local client = vim.lsp.get_client_by_id(args.data.client_id)
+          if client and client.server_capabilities.inlineCompletionProvider then
+            inline_completion.enable({
+              debounce = 150,
+              show_on_undo = false,
+            })
+          end
+        end,
+      })
+    end,
+  },
 }
