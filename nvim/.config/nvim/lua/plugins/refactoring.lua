58a8d62fec81ae433f36f9ddfdc689081fd3dee0
wulymammoth <861147+wulymammoth@users.noreply.github.com>
2026-04-20 23:19:19 -0400

    bugfix(nvim): error with refactoring.nvim's new dep on lewis6991/async.nvim

---
+++ nvim/.config/nvim/lua/plugins/refactoring.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "ThePrimeagen/refactoring.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
+      "lewis6991/async.nvim",
+    },
+  },
+}
